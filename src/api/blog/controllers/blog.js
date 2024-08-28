622ae184d5c980aeb46ad979562f20ca0ac12834
Konkamon Sion <bigkonkamon@gmail.com>
2024-08-28 19:46:06 +0700

    Fetching Blogs Item by Slug Instead of an ID

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -1,9 +1,33 @@
-'use strict';
+"use strict";
 
 /**
  * blog controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::blog.blog');
+// module.exports = createCoreController('api::blog.blog');
+module.exports = createCoreController("api::blog.blog", ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+
+    strapi.db.query("api::blog.blog");
+
+    return { data, meta };
+  },
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const post = await strapi.entityService.findMany("api::blog.blog", query);
+
+    // const sanitizedEntity = await this.sanitizeOutput(post);
+
+    return this.transformResponse(post[0]);
+  },
+}));
