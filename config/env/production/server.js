451daf7fda1a463716af52757fd2b579f5e40082
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-01 02:34:23 +0700

    feat: Added '/admin' URL

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     url: 'https://konkamon-strapi.azurewebsites.net',
+    admin: {
+        url: "/admin"
+      }
   });
