e94437b6c540358ce1c0ca03d82dddbf4ff9c90f
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-01 03:11:03 +0700

    ci: :sparkles: Updated Workflow once again

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+echo "Removing src, config"
+rm -rf /home/site/wwwroot/src
+rm -rf /home/site/wwwroot/config
+rsync -arv --no-o --no-g --ignore-existing --size-only  ./ /home/site/wwwroot
+echo "Source sync done."
