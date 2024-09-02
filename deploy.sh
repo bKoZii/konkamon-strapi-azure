a8d34208c26b13853ae82ebf3177f4aeed5e2565
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-02 13:20:00 +0700

    Make this also delete package.json
    since package.json is not updated on Azure

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 echo "Removing src, config"
 rm -rf /home/site/wwwroot/src
 rm -rf /home/site/wwwroot/config
+rm -rf /home/site/wwwroot/package.json
 rsync -arv --no-o --no-g --ignore-existing --size-only  ./ /home/site/wwwroot
 echo "Source sync done."
