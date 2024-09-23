2cabe4e69b7893aa35a7853d81e98a2b2977b091
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-23 13:41:07 +0700

    Revert "chore(deploy.sh): :wrench: removing node_module"
    This reverts commit 51cd901f1a125b60359c4f87cdf75bf1beb84e24.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@ rm -rf /home/site/wwwroot/src
 rm -rf /home/site/wwwroot/config
 rm -rf /home/site/wwwroot/package.json
 rm -rf /home/site/wwwroot/build
-rm -rf /home/site/wwwroot/node_modules
 rsync -arv --no-o --no-g --ignore-existing --size-only  ./ /home/site/wwwroot
 echo "Source sync done."
